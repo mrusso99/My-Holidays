5271ca343aeba66462a3dfdef72eacf52895522b
RoccoAliberti97 <roccoaliberti53@gmail.com>
2021-12-21 10:14:28 +0100

    Merge branch 'main' into raliberti97_branch

--- lib/ui/profile_screen.dart
+++ lib/ui/profile_screen.dart
@@ -1,4 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:my_holidays/languages/languageLocalizations.dart';
+
+import 'profile_page.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({Key? key}) : super(key: key);
@@ -33,10 +38,9 @@ class _ProfileScreenState extends State<ProfileScreen> {
     return Scaffold(
       body: Center(
         child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-          Card(
-            child: ListTile(
+           ListTile(
                 leading: Icon(Icons.account_box),
-                title: Text('Profilo'),
+                title: Text(LanguageLocalizations.of(context).profile),
                 onTap: () {
                   if (true) {
                     //todo check login
@@ -44,21 +48,20 @@ class _ProfileScreenState extends State<ProfileScreen> {
                     }
                   }
                 ),
-              ListTile(
+               ListTile(
                 leading: const Icon(Icons.settings),
                 title: Text(LanguageLocalizations.of(context).settings),
                 onTap: () {
                   Navigator.pushNamed(context, 'Settings');
                 },
               ),
-    ListTile(
+          ListTile(
               leading: Icon(Icons.settings),
               title: Text('Self Check-in'),
               onTap: () {
                 Navigator.pushNamed(context, 'Checkin');
               },
             ),
-          ),
         ]),
       ),
     );
