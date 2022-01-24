0ef20b652a0d4b72d8b8318d18cf7b59370f3392
carmineiannotti <carmineiannotti@gmail.com>
2022-01-24 16:31:09 +0100

    flask python fix new ganache

--- FelixCoin/migrations/2_deploy_contracts.js
+++ FelixCoin/migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var Token = artifacts.require("./FelixCoin.sol");
 
 module.exports = function(deployer) {
-    deployer.deploy(Token,"0xbb052D09319ACCd7C65A8cAae95fCe00d63e7eF5","FelixCoin","FELX",24444);
+    deployer.deploy(Token,"0x87c81230BA0f714cB638168DF5227db3437468c8","FelixCoin","FELX",24444);
 };
